b8d87c7e43eb9f8b46d5cd415ee7b5a730d88c15
girishankar.kb <girishankar.kb@ge.com>
2017-11-08 12:45:00 +0000

    resolved src path link error

--- xsdk/Debug/hiredis/subdir.mk
+++ xsdk/Debug/hiredis/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/home/deploy/git/hiredis/async.c \
-/home/deploy/git/hiredis/dict.c \
-/home/deploy/git/hiredis/hiredis.c \
-/home/deploy/git/hiredis/net.c \
-/home/deploy/git/hiredis/read.c \
-/home/deploy/git/hiredis/sds.c \
-/home/deploy/git/hiredis/test.c 
+/home/deploy/git/hir/hiredis/async.c \
+/home/deploy/git/hir/hiredis/dict.c \
+/home/deploy/git/hir/hiredis/hiredis.c \
+/home/deploy/git/hir/hiredis/net.c \
+/home/deploy/git/hir/hiredis/read.c \
+/home/deploy/git/hir/hiredis/sds.c \
+/home/deploy/git/hir/hiredis/test.c 
 
 OBJS += \
 ./hiredis/async.o \
@@ -32,49 +32,49 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-hiredis/async.o: /home/deploy/git/hiredis/async.c
+hiredis/async.o: /home/deploy/git/hir/hiredis/async.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-hiredis/dict.o: /home/deploy/git/hiredis/dict.c
+hiredis/dict.o: /home/deploy/git/hir/hiredis/dict.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-hiredis/hiredis.o: /home/deploy/git/hiredis/hiredis.c
+hiredis/hiredis.o: /home/deploy/git/hir/hiredis/hiredis.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-hiredis/net.o: /home/deploy/git/hiredis/net.c
+hiredis/net.o: /home/deploy/git/hir/hiredis/net.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-hiredis/read.o: /home/deploy/git/hiredis/read.c
+hiredis/read.o: /home/deploy/git/hir/hiredis/read.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-hiredis/sds.o: /home/deploy/git/hiredis/sds.c
+hiredis/sds.o: /home/deploy/git/hir/hiredis/sds.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-hiredis/test.o: /home/deploy/git/hiredis/test.c
+hiredis/test.o: /home/deploy/git/hir/hiredis/test.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM A53 Linux gcc compiler'
 	aarch64-linux-gnu-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
